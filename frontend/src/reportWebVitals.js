76171f471d8abe6692485253da972abdfd1b8bea
vanessa-196 <vanessa.umenze@acity.edu.gh>
2025-04-10 20:01:26 +0000

    Fix: Re-added frontend folder after cleaning up .gitignore and .git

---
+++ frontend/src/reportWebVitals.js
@@ -0,0 +1,13 @@
+// src/reportWebVitals.js
+const reportWebVitals = (onPerfEntry) => {
+    if (onPerfEntry && onPerfEntry instanceof Function) {
+      import('web-vitals').then(({ getCLS, getFID, getLCP }) => {
+        getCLS(onPerfEntry);
+        getFID(onPerfEntry);
+        getLCP(onPerfEntry);
+      });
+    }
+  };
+  
+  export default reportWebVitals;
+  
